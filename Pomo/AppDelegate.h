29465624664830998832038a96f245fefc805470
Guido Bouman <m@guido.vc>
2014-01-08 21:24:35 +0100

    Initial Commit

---
+++ Pomo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Pomo
+//
+//  Created by Guido Bouman on 08/01/14.
+//  Copyright (c) 2014 Guido Bouman. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Pomo/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  Pomo
+//
+//  Created by Guido Bouman on 08/01/14.
+//  Copyright (c) 2014 Guido Bouman. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
